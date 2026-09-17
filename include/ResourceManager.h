2ffa3d30f6b791cf2c84a922c97d39a904a7de20
rooseveltngwatemoh <ngwatemohex@gmail.com>
2026-09-17 17:06:35 -0500

    Update ResourceManager.h

--- include/ResourceManager.h
+++ include/ResourceManager.h
@@ -1,1 +1,30 @@
+#ifndef RESOURCEMANAGER_H
+#define RESOURCEMANAGER_H
 
+#include "Resource.h"
+#include <vector>
+
+#include <string>
+
+class ResourceManager {
+private:
+    std::vector<Resource> resources;
+    std::string dataSourceFile;
+
+public:
+    ResourceManager(const std::string& filename = "data/resources.txt");
+
+    // File oper
+    bool loadResources();
+    bool saveResources() const;
+
+    //  Display Operations
+    void displayAllResources() const;
+    void displayAvailableResources() const;
+
+    // helpers
+    Resource* getResourceById(const std::string& id);
+    bool resourceExists(const std::string& id) const;
+};
+
+#endif
