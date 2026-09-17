360f0a87674b648540ff267a6ec38849ba4dafac
rooseveltngwatemoh <ngwatemohex@gmail.com>
2026-09-17 17:24:18 -0500

    Update WaitQueue.h

--- include/WaitQueue.h
+++ include/WaitQueue.h
@@ -1,7 +1,6 @@
 #ifndef WAITQUEUE_H
 #define WAITQUEUE_H
 
-
 #include "WaitRequest.h"
 
 class WaitQueue {
@@ -15,19 +14,19 @@ private:
             next = nullptr;
         }
     };
-
     Node* front;
     Node* rear;
     int count;
+
 public:
     WaitQueue();
     ~WaitQueue();
     
-    // Copy constructor and assignment operator to handle vectors of queues
+   
     WaitQueue(const WaitQueue& other);
     WaitQueue& operator=(const WaitQueue& other);
 
-    //Queue oper
+    // Queue oper
     void enqueue(const WaitRequest& request);
     bool dequeue(WaitRequest& out);
     bool peek(WaitRequest& out) const;
@@ -35,16 +34,6 @@ public:
     int size() const;
     
     void displayAll() const;
-
-    // This template allows the lambda function in your saveToFile method to work
-    template <typename Func>
-    void forEach(Func action) const {
-        Node* current = front;
-        while (current != nullptr) {
-            action(current->data);
-            current = current->next;
-        }
-    }
 };
 
 #endif 
