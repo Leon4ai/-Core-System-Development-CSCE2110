d86241dc727853091cb3042daea4bbb6e4a8e017
Leon4ai <abdullah.ahmed.zaigham07@gmail.com>
2026-09-19 11:21:13 -0500

    Add search and sort functions for Resource

--- include/Algorithms.h
+++ include/Algorithms.h
@@ -1,1 +1,23 @@
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
 
+#include <vector>
+
+#include "Resource.h"
+
+int linearSearchResourceById(
+    const std::vector<Resource>& resources,
+    const std::string& resourceId
+);
+
+int binarySearchResourceById(
+    const std::vector<Resource>& resources,
+    const std::string& resourceId
+);
+
+void quickSortResources(
+    std::vector<Resource>& resources,
+    int criterion
+);
+
+#endif
